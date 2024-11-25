5bc95a438135aa6c6d51f91ff8426d55c740d740
Asubek <asubekerakyluly@gmail.com>
2024-11-25 12:10:50 +0500

    exam 25.11

---
+++ script.js
@@ -0,0 +1,90 @@
+//  1. Тапсырма: Қарапайым калкулятор
+
+// var num1 = +prompt("Tenter 1st number:")
+// var simbol = prompt("/  *   -   + ")
+// var num2 = +prompt("Enter 2nd number:")
+// var result = 0
+
+
+// switch(simbol){
+//     case "/":
+//         result += num1 / num2
+//     break;
+//     case "*":
+//         result += num1 * num2
+//     break;
+//     case "-":
+//         result += num1 - num2
+//     break;
+//     case "+":
+//         result += num1 + num2
+//     break;
+//     default:
+//         alert("Қате енгізу!")
+
+// }
+
+// alert(result)
+
+
+
+// 2. Тапсырма: Супермаркеттің жеңілдіктері
+
+// var baga = +prompt("Заттың бағасы:")
+// var zat = +prompt("Қанша зат сатып алғыңыз келеді:")
+// var total = 0
+
+// if(baga > 5000){
+//     total += (baga / 100) * 10
+// }
+// else if(baga > 10000){
+//     total += (baga / 100) * 20
+// }
+// else if(baga > 20000){
+//     total += (baga / 100) * 30
+// }
+
+// alert("Барлығы "+ baga + "₸ болды" + "\n" + "Жеңілдікпен " + (baga - total) +"₸ болды" + "\n" + zat +" осынша зат алды")
+
+
+
+
+// 3. Тапсырма: Пайдаланушыға кездейсоқ сан жасыру
+
+// var pass = 65
+// var tabu 
+
+// while(true){
+//     tabu = +prompt("Жасырын сан табу үшін сан жазыңыз:")
+//     if(tabu == pass){
+//         alert("Сіз жасырын кодты таптыңыз!!!")
+//         break;
+//     }
+//     if(tabu <= 70 && tabu >= 65){
+//         alert("Сәл жоғары")
+//     }
+//     if(tabu >= 60 && tabu <= 65){
+//         alert("Сәл төмен")
+//     }
+//     else if(tabu > 70 ){
+//         alert("Жай жоғары")
+//     }
+//     else if(tabu < 60){
+//         alert("Жай төмен")
+//     }
+
+// }
+
+
+// bonus task
+
+let a = 15
+let b = 20
+
+
+
+console.log(a);
+console.log(b)
+
+
+
